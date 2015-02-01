91a8a6122aeaddcd6a4ed2cc3ec55434ee670253
ForgeAhead <ForgeAhead@users.noreply.github.com>
2015-02-01 12:03:59 +1100

    Create Reddit-retriever_v2.py
    Now doesnt msg you but opens up a new tab with the giveaway in a browser.

---
+++ Reddit-retriever_v2.py
@@ -0,0 +1,32 @@
+#!usr/bin/python
+
+import praw
+import time
+import Tkinter
+import tkMessageBox
+
+
+
+r = praw.Reddit('Keyword search in new posts per subreddit by /u/VeiledFortune')
+
+r.login()
+already_seen = []
+
+keyword_list = ['Giveaway', 'giveaway', 'tip', 'Tip']
+
+while True:
+	subreddit = r.get_subreddit('dogecoin+Bitcoin')
+	for submission in subreddit.get_new(limit=10):
+		op_text = submission.title 
+		has_keywords = any(word in op_text for word in keyword_list)
+		if has_keywords and submission.id not in already_seen:
+			already_seen.append(submission.id)
+			top = Tkinter.Tk()
+			def hello():
+   				tkMessageBox.showinfo("Found one", "Found one")
+
+			B1 = Tkinter.Button(top, text = "Found one", command = command=webbrowser.open(submission.short_link))
+			B1.pack()
+
+			top.mainloop()
+	time.sleep(300)
